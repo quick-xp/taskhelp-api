20de52b0622fab6f9a20a43f37f8984e765f3e59
r.nishio <quick.xp@gmail.com>
2018-12-23 20:21:16 +0900

    dev

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
-import * as Express from 'express';
+import * as Express from 'express'
 
-const app = Express();
+const app = Express()
 
-app.get(
-  '/',
-  (req: Express.Request, res: Express.Response)=>{
-    return res.send('Hello Node')
-  });
+app.get('/', (req: Express.Request, res: Express.Response) => {
+  return res.send('Hello Node')
+})
 
-app.listen(
-  3000,
-  () => {
-    console.log("listening on port 3000")
-  }
-);
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
 
-export default app;
+export default app
