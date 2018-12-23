d059536f52417da6027e6ca1111d92ea03052428
r.nishio <quick.xp@gmail.com>
2018-12-23 20:21:40 +0900

    dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,38 +1,38 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/server.ts",
-  target: "node",
-  externals: [nodeExternals()],
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        loader: "tslint-loader",
-        test: /\.ts$/,
-        exclude: [/node_modules/],
-        options: {
-          emitErrors: true
-        }
-      },
-      {
-        loader: "ts-loader",
-        test: /\.ts$/,
-        exclude: [/node_modules/],
-        options: {
-          configFile: "tsconfig.dev.json"
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist")
-  }
-};
+    mode: 'development',
+    entry: './src/server.ts',
+    target: 'node',
+    externals: [nodeExternals()],
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                options: {
+                    emitErrors: true
+                }
+            },
+            {
+                loader: 'ts-loader',
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                options: {
+                    configFile: 'tsconfig.dev.json'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
