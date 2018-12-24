154f8da84dbf262b95cf2fd181b0235570d3850a
r.nishio <quick.xp@gmail.com>
2018-12-24 20:35:18 +0900

    dev

---
+++ src/interfaces/serializers/TaskSerializer.ts
@@ -0,0 +1,19 @@
+const _serializeSingleTask = task => {
+  return {
+    id: task.getId,
+    title: task.getTitle,
+    description: task.getDescription
+  }
+}
+
+module.exports = class {
+  serialize(data) {
+    if (!data) {
+      throw new Error('expect data to be not undefined nor null')
+    }
+    if (Array.isArray(data)) {
+      return data.map(_serializeSingleTask)
+    }
+    return _serializeSingleTask
+  }
+}
