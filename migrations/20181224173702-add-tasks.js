7697a8e8c7c4e004fb7bc14808b4b09dd6859292
r.nishio <quick.xp@gmail.com>
2018-12-25 02:46:47 +0900

    dev

--- migrations/20181224173702-add-tasks.js
+++ migrations/20181224173702-add-tasks.js
@@ -17,7 +17,8 @@ exports.setup = function(options, seedLink) {
 exports.up = function(db) {
   return db.createTable('tasks', {
     id: { type: 'int', primaryKey: true },
-    name: 'string'
+    name: 'string',
+    description: 'string'
   })
 }
 
