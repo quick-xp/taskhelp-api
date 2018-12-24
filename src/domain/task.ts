ee19e3b25f2cab83048c919c17b68d3c59ee113b
r.nishio <quick.xp@gmail.com>
2018-12-24 16:43:39 +0900

    dev

--- src/domain/task.ts
+++ src/domain/task.ts
@@ -1,6 +1,7 @@
-export class Task {
+class Task {
+  private _id: number
   private _title: string
-  private _description: string
+  private _description: Text
 
   get title() {
     return this._title
@@ -8,5 +9,11 @@ export class Task {
 
   get description() {
     return this._description
+  }
+
+  constructor(id: number = null, title: string, description: Text) {
+    this._id = id
+    this._title = title
+    this._description = description
   }
 }
