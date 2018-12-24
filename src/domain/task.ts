83da884bdebf128651a49b70574f1d5eff7d799b
r.nishio <quick.xp@gmail.com>
2018-12-24 16:30:11 +0900

    dev

---
+++ src/domain/task.ts
@@ -0,0 +1,12 @@
+export class Task {
+  private _title: string
+  private _description: string
+
+  get title() {
+    return this._title
+  }
+
+  get description() {
+    return this._description
+  }
+}
